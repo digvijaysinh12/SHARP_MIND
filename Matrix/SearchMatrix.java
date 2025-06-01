cfbed49371193a2118ffd9e8a985278b727cc6f3
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-02 00:51:27 +0530

    Search 2D Matrix

---
+++ Matrix/SearchMatrix.java
@@ -0,0 +1,40 @@
+
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int i = 0;
+        int j = cols - 1;
+
+        while (i < rows && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+        SearchMatrix sm = new SearchMatrix();
+        int[][] matrix = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+        int target = 5;
+        System.out.println("Is target found? " + sm.searchMatrix(matrix, target));
+        //true
+    }
+}
