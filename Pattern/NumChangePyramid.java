243a1a955899a99773ab079a9708d8e861ef1daf
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-07-03 00:40:48 +0530

    Number-Changing Pyramid Pattern

---
+++ Pattern/NumChangePyramid.java
@@ -0,0 +1,11 @@
+public class NumChangePyramid {
+        public static void main(String[] args) {
+        int c =1;    
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(c++);
+            }
+            System.out.println();
+        }
+    }
+}
