0310616d83f6573eb2394c4833c8c23dfd22fcf0
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-07-03 00:48:59 +0530

    With another method

--- Pattern/zeroOneTriangle.java
+++ Pattern/zeroOneTriangle.java
@@ -12,5 +12,16 @@ public class zeroOneTriangle {
             }
             System.out.println();
         }
+
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print(1);
+                }else{
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
     }
 }
