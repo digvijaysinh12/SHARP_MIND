9ebb9e1583afe297566ef7e3a5eeb07343e63da7
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-07-07 17:07:24 +0530

    ..

--- Pattern/Pattern7.java
+++ Pattern/Pattern7.java
@@ -8,11 +8,11 @@ public class Pattern7 {
         for(int i=1;i<=2*n-1;i++){
             if(i>n){
                 for(int j=1;j<=2*n-i;j++){
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
             }else{
                 for(int j=1;j<=i;j++){
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
             }
             System.out.println();
