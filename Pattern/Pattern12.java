1c846ec2c303e5628d43936b0791f55f2f093d69
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-07-07 17:54:02 +0530

    Successfully print pattern 11

---
+++ Pattern/Pattern12.java
@@ -0,0 +1,35 @@
+// 10.      *
+//         * *
+//        * * *
+//       * * * *
+//      * * * * *
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n){
+        for(int rows =1;rows<=n;rows++){
+            //spaces
+            for(int space=1;space<=n-rows;space++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int cols=1;cols<=2*rows-1;cols++){
+                if(cols%2==0){
+                    System.out.print(" ");
+                }else{
+                System.out.print("*");
+                }
+            }
+            //spaces
+            for(int space=1;space<=n-rows;space++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
